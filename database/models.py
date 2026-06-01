f58c68fbad4cc271f30af5f73ea626bc878d8e96
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-06-01 23:51:37 +0530

    all models done

--- database/models.py
+++ database/models.py
@@ -1,2 +1,75 @@
 from initialization import Base
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
+from sqlalchemy import String, DateTime, func, Text, ForeignKey, UniqueConstraint, Integer, Boolean
+from datetime import datetime, timezone, timedelta
+from sqlalchemy import Enum
+from typing import Optional
+import enum
 
+class SubscriptionStatus(enum.Enum):
+    active = "active"
+    inactive = "inactive"
+
+class UserModel(Base):
+	__tablename__ = 'users'
+	# individual columns
+	user_id : Mapped[str] = mapped_column(String(50),primary_key=True, index=True)
+	username : Mapped[str] = mapped_column(String(50))
+	profile_pic_url : Mapped[str] = mapped_column(Text)
+	encrypted_access_token : Mapped[str] = mapped_column(Text)
+	token_expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+	created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default= func.now())
+
+	# relationships
+	subscription = Relationship('SubscriptionModel',back_populates='user')
+	rules = Relationship('RuleModel',back_populates='user')
+
+class SubscriptionModel(Base):
+	__tablename__ = 'subscriptions'
+	# Primary Key 
+	user_id : Mapped[str] = mapped_column(String(50),ForeignKey('users.user_id',ondelete='CASCADE'),index=True,primary_key=True)
+
+	# other columns
+	status : Mapped[SubscriptionStatus] = mapped_column(Enum(SubscriptionStatus))
+	next_billing_date : Mapped[datetime] = mapped_column(DateTime(timezone=True),default= lambda : datetime.now(timezone.utc) + timedelta(days=7))
+
+	#relationships
+	user = Relationship('UserModel',back_populates='subscription',passive_deletes=True)
+
+class RuleModel(Base):
+	__tablename__ = 'rules'
+
+	__table_args__ = (
+   	UniqueConstraint("media_id", "catchphrase"),
+	)
+	
+	# Primary Key
+	id : Mapped[int] = mapped_column(Integer,primary_key=True,autoincrement=True)
+
+	media_id: Mapped[str] = mapped_column(String(100),nullable=False,index=True)
+	catchphrase: Mapped[str] = mapped_column(String(100),nullable=False,index=True)
+	dm_message: Mapped[str] = mapped_column(Text,nullable=False)
+	reply_message: Mapped[Optional[str]] = mapped_column(Text,nullable=True,default=None)
+	is_active: Mapped[bool] = mapped_column(Boolean,default=True)
+	created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+	count : Mapped[int] = mapped_column(Integer, server_default='0')
+
+	# Foreign Keys
+	user_id : Mapped[str] = mapped_column(String(50), ForeignKey('users.user_id',ondelete='CASCADE'),index=True)
+
+	# Relationships
+	user = Relationship('UserModel',back_populates='rules')
+	dms = Relationship('DMLogsModel',back_populates='rule')
+
+class DMLogsModel(Base):
+	__tablename__ = 'dm_logs'
+
+	rule_id : Mapped[int] = mapped_column(Integer, ForeignKey('rules.id',ondelete='CASCADE'),index=True)
+	id: Mapped[int] = mapped_column(Integer,primary_key=True,autoincrement=True)
+	commenter_ig_id: Mapped[str] = mapped_column(String(100),nullable=False,index=True)
+	media_id: Mapped[str] = mapped_column(String(100),nullable=False,index=True)
+	comment_id: Mapped[str] = mapped_column(String(100),nullable=False,unique=True,index=True)
+	sent_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
+
+	#Foreign Keys
+	rule = Relationship('RuleModel',back_populates='dms')
