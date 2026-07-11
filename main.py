5bed0157e67a70eb87d680ed4fec796f56d30ef7
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-07-11 09:15:54 +0530

    fixed cron bug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routers.payments import router as payments_router
 from routers.user import router as user_router
 from routers.instagram.main_router import router as instagram_router
 from routers.rules import router as rules_router
-from utils.background_tasks import scheduler, refresh_instagram_tokens, wipe_deleted_users
+from utils.background_tasks import scheduler, refresh_instagram_tokens, wipe_deleted_users, publish_scheduled_posts
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
@@ -17,9 +17,9 @@ limiter = Limiter(key_func=get_remote_address)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    scheduler.add_job(refresh_instagram_tokens,'cron',hour=24,minute=0)
-    scheduler.add_job(wipe_deleted_users,'cron',hour=24,minute=0)
-    scheduler.add_job()
+    scheduler.add_job(refresh_instagram_tokens,'cron',hour=0,minute=0)
+    scheduler.add_job(wipe_deleted_users,'cron',hour=0,minute=0)
+    scheduler.add_job(publish_scheduled_posts, 'interval',minutes=2)
     scheduler.start()
     yield
     scheduler.shutdown()
