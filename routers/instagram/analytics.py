ed9f4afd333adb1310ca73e5ff5daee540f0dc47
AarushSrivatsa <pitlaaarushsrivatsa@gmail.com>
2026-07-11 15:09:24 +0530

    analytics is even more dope

--- routers/instagram/analytics.py
+++ routers/instagram/analytics.py
@@ -6,36 +6,47 @@ from database.models import UserModel
 from utils.token_handling import get_current_user
 from utils.http_client import client
 from utils.encryption import decrypt
+from typing import Optional
 
 router = APIRouter(prefix='/analytics')
 
 @router.get('/account')
 async def get_account_insights(
     period: str = 'day',
+    metric_type: str = 'total_value',
+    since: Optional[int] = None,
+    until: Optional[int] = None,
     user: UserModel = Depends(get_current_user)
 ):
     if period not in ('day', 'week', 'month'):
-        raise HTTPException(status_code=400, detail='Invalid period. Use day, week or month')
-    
+        raise HTTPException(status_code=400, detail='Invalid period')
+
     access_token = decrypt(user.encrypted_instagram_access_token)
-    
+
+    params = {
+        'metric': 'reach,views,accounts_engaged,total_interactions,follows_and_unfollows,shares,saves,likes,comments',
+        'period': period,
+        'metric_type': metric_type,
+        'access_token': access_token
+    }
+    if since:
+        params['since'] = since
+    if until:
+        params['until'] = until
+
     response = await client.get(
         f'https://graph.instagram.com/v25.0/{user.user_id}/insights',
-        params={
-            'metric': 'impressions,reach,profile_views',
-            'period': period,
-            'access_token': access_token
-        }
+        params=params
     )
     return response.json()
 
-@router.get('/posts')
-async def get_all_posts_insights(
+@router.get('/media')
+async def get_all_media_insights(
     user: UserModel = Depends(get_current_user)
 ):
     access_token = decrypt(user.encrypted_instagram_access_token)
 
-    # Fetch all media first
+    # Fetch all media
     media_response = await client.get(
         f'https://graph.instagram.com/v25.0/{user.user_id}/media',
         params={
@@ -45,13 +56,17 @@ async def get_all_posts_insights(
     )
     media_list = media_response.json().get('data', [])
 
-    # Fetch insights for each post
     results = []
     for media in media_list:
+        if media['media_type'] == 'VIDEO':
+            metrics = 'reach,views,likes,comments,saves,shares,total_interactions,ig_reels_avg_watch_time,ig_reels_video_view_total_time,reels_skip_rate,reposts'
+        else:
+            metrics = 'reach,views,likes,comments,saves,shares,total_interactions,profile_visits,follows,reposts'
+
         insights_response = await client.get(
             f'https://graph.instagram.com/v25.0/{media["id"]}/insights',
             params={
-                'metric': 'engagement,impressions,reach',
+                'metric': metrics,
                 'access_token': access_token
             }
         )
@@ -64,17 +79,23 @@ async def get_all_posts_insights(
 
     return results
 
-@router.get('/post/{media_id}')
-async def get_post_insights(
+@router.get('/media/{media_id}')
+async def get_media_insights(
     media_id: str,
+    media_type: str = 'IMAGE',
     user: UserModel = Depends(get_current_user)
 ):
     access_token = decrypt(user.encrypted_instagram_access_token)
 
+    if media_type == 'VIDEO':
+        metrics = 'reach,views,likes,comments,saves,shares,total_interactions,ig_reels_avg_watch_time,ig_reels_video_view_total_time,reels_skip_rate,reposts'
+    else:
+        metrics = 'reach,views,likes,comments,saves,shares,total_interactions,profile_visits,follows,reposts'
+
     response = await client.get(
         f'https://graph.instagram.com/v25.0/{media_id}/insights',
         params={
-            'metric': 'engagement,impressions,reach',
+            'metric': metrics,
             'access_token': access_token
         }
     )
